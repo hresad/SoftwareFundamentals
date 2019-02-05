d5e1a90a50b81530fa03708ff85e12c626df673f
Rešad <resad.hajdarpasic1@apeiron-edu.eu>
2019-02-05 17:27:32 +0100

    Added MyStack

---
+++ Stack/Stack/MyStack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class MyStack<T>
+    {
+        private T[] array;
+        private int currentIndex;
+        public MyStack()
+        {
+            array = new T[3];
+            currentIndex = -1;
+        }
+        private void Resize()
+        {
+            T[] newArray = new T[array.Length * 2];
+            int i = 0;
+            foreach (T item in array)
+            {
+                newArray[i++] = item;
+            }
+            array = newArray;
+        }
+        public void Push (T item)
+        {
+            currentIndex++;
+            if (currentIndex == array.Length)
+            {
+                Resize();
+            }
+            array[currentIndex] = item;
+        }
+        public T Pop ()
+        {
+            if (currentIndex >= 0)
+            {
+                T item = array[currentIndex];
+                currentIndex--;
+                return item;
+            } else
+            {
+                throw new IndexOutOfRangeException();
+            }
+            
+        }
+        public int Length()
+        {
+            return currentIndex + 1;
+        }
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<String> stack = new MyStack<string>();
+            stack.Push("Prvi string");
+            stack.Push("Drugi string");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine();
+
+            stack.Push("Novi Prvi string");
+            stack.Push("Novi Drugi string");
+            stack.Push("Novi Treci string");
+            stack.Push("Novi Cetvrti string");
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
